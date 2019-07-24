1a5a02c3257b05a041d759aed79d2995e177ac96
oyincode <oyindadavid18@gmail.com>
2019-07-24 13:48:51 +0100

    receive toastr when new notification

--- client/src/components/core/Toolbar.vue
+++ client/src/components/core/Toolbar.vue
@@ -1,22 +1,8 @@
 <template>
-  <v-toolbar
-    id="core-toolbar"
-
-    flat
-    prominent
-    style="background: #eee;"
-  >
+  <v-toolbar id="core-toolbar" flat prominent style="background: #eee;">
     <div class="v-toolbar-title">
-      <v-toolbar-title
-        class="tertiary--text font-weight-light"
-      >
-        <v-btn
-          v-if="responsive"
-          class="default v-btn--simple"
-          dark
-          icon
-          @click.stop="onClickBtn"
-        >
+      <v-toolbar-title class="tertiary--text font-weight-light">
+        <v-btn v-if="responsive" class="default v-btn--simple" dark icon @click.stop="onClickBtn">
           <v-icon>mdi-view-list</v-icon>
         </v-btn>
         {{ title }}
@@ -25,11 +11,7 @@
 
     <v-spacer />
     <v-toolbar-items>
-      <v-flex
-        align-center
-        layout
-        py-2
-      >
+      <v-flex align-center layout py-2>
         <v-text-field
           v-if="responsiveInput"
           class="mr-4 mt-2 purple-input"
@@ -37,53 +19,25 @@
           hide-details
           color="purple"
         />
-        <router-link
-          v-ripple
-          class="toolbar-items"
-          to="/"
-        >
+        <router-link v-ripple class="toolbar-items" to="/">
           <v-icon color="tertiary">mdi-view-dashboard</v-icon>
         </router-link>
-        <v-menu
-          bottom
-          left
-          content-class="dropdown-menu"
-          offset-y
-          transition="slide-y-transition">
-          <router-link
-            v-ripple
-            slot="activator"
-            class="toolbar-items"
-            to="/notifications"
-          >
-            <v-badge
-              color="error"
-              overlap
-            >
-              <template slot="badge">
-                {{ notifications.length }}
-              </template>
+        <v-menu bottom left content-class="dropdown-menu" offset-y transition="slide-y-transition">
+          <router-link v-ripple slot="activator" class="toolbar-items" to="/notifications">
+            <v-badge color="error" overlap>
+              <template slot="badge">{{ notifications.length }}</template>
               <v-icon color="tertiary">mdi-bell</v-icon>
             </v-badge>
           </router-link>
           <v-card>
             <v-list dense>
-              <v-list-tile
-                v-for="notification in notifications"
-                @click="onClick"
-              >
-                <v-list-tile-title
-                  v-text="notification"
-                />
+              <v-list-tile v-for="notification in notifications" @click="onClick">
+                <v-list-tile-title v-text="notification" />
               </v-list-tile>
             </v-list>
           </v-card>
         </v-menu>
-        <router-link
-          v-ripple
-          class="toolbar-items"
-          to="/user-profile"
-        >
+        <router-link v-ripple class="toolbar-items" to="/user-profile">
           <v-icon color="tertiary">mdi-account</v-icon>
         </router-link>
       </v-flex>
@@ -92,18 +46,15 @@
 </template>
 
 <script>
+import { mapMutations } from "vuex";
 
-import {
-  mapMutations
-} from 'vuex'
+import { db } from "./db";
 
-import { db } from './db'
+import toastr from "toastr";
 
 const API_URL = "http://localhost:3000/api/getNotifications";
 
-
 export default {
-
   data: () => ({
     notifications: [],
     title: null,
@@ -111,62 +62,53 @@ export default {
     responsiveInput: false
   }),
 
-  firebase:{
-    notifications : db.ref('notifications')
+  firebase: {
+    notifications: db.ref("notifications")
   },
 
   watch: {
-    '$route' (val) {
-      this.title = val.name
-    },
-    // id: {
-    //   // call it upon creation too
-    //   immediate: true,
-    //   handler(id) {
-    //     this.$rtdbBind('user', users.child(id))
-    //   },
-    // },
+    $route(val) {
+      this.title = val.name;
+    }
   },
 
-  created () {
-    this.onResponsiveInverted()
-    window.addEventListener('resize', this.onResponsiveInverted)
-    console.log(this.notifications);
-    // fetch(API_URL)
-    //   .then(response => response.json())
-    //   .then(result => {
-    //     for(let data in result.data){
-    //       this.notifications.push(data);
-    //     }
-      // });
+  created() {
+    this.onResponsiveInverted();
+    window.addEventListener("resize", this.onResponsiveInverted);
+    //console.log(this.notifications);
+
+    db.ref("notifications").endAt().limitToLast(1).on("child_added", function(snap) {
+      console.log(snap.val())
+      toastr.success("You have received a new notification");
+    })
   },
-  beforeDestroy () {
-    window.removeEventListener('resize', this.onResponsiveInverted)
+  beforeDestroy() {
+    window.removeEventListener("resize", this.onResponsiveInverted);
   },
 
   methods: {
-    ...mapMutations('app', ['setDrawer', 'toggleDrawer']),
-    onClickBtn () {
-      this.setDrawer(!this.$store.state.app.drawer)
+    ...mapMutations("app", ["setDrawer", "toggleDrawer"]),
+    onClickBtn() {
+      this.setDrawer(!this.$store.state.app.drawer);
     },
-    onClick () {
+    onClick() {
       //
     },
-    onResponsiveInverted () {
+    onResponsiveInverted() {
       if (window.innerWidth < 991) {
-        this.responsive = true
-        this.responsiveInput = false
+        this.responsive = true;
+        this.responsiveInput = false;
       } else {
-        this.responsive = false
-        this.responsiveInput = true
+        this.responsive = false;
+        this.responsiveInput = true;
       }
     }
   }
-}
+};
 </script>
 
 <style>
-  #core-toolbar a {
-    text-decoration: none;
-  }
+#core-toolbar a {
+  text-decoration: none;
+}
 </style>
